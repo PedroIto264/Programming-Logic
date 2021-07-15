4d5601766d0a3861b5ad55c383e4ec44c99f22e3
Pedro Ito <67910276+PedroIto264@users.noreply.github.com>
2021-07-15 13:16:38 -0300

    Add files via upload

---
+++ URIJudge1036.py
@@ -0,0 +1,10 @@
+while True:
+    patos = int(input())
+    if patos>0:
+        patosR = patos - 1
+        print(patosR)
+    if patos == 0:
+        print(patos)
+    else:
+        if patos == -1:
+            break
